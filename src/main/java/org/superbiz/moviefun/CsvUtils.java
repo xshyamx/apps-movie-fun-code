9bd04a6ede7d7744e177fd7e7d0d7771dfc8d354
shyam <shyamsundar.shanmugam@wipro.com>
2018-04-05 11:49:14 +0800

    file system assignment

--- src/main/java/org/superbiz/moviefun/CsvUtils.java
+++ src/main/java/org/superbiz/moviefun/CsvUtils.java
@@ -6,25 +6,15 @@ import com.fasterxml.jackson.databind.ObjectReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class CsvUtils {
 
-    public static String readFile(String path) {
-        try {
-            Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
-
-            if (scanner.hasNext()) {
-                return scanner.next();
-            } else {
-                return "";
-            }
-
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+    public static InputStream readFile(String path) {
+        return ClassLoader.getSystemResourceAsStream(path);
     }
 
     public static <T> List<T> readFromCsv(ObjectReader objectReader, String path) {
