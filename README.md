fd465882ce79165d56d6e3e5c8aa79d94db7ab49
Manoj Potla <potlamf@gmail.com>
2018-04-09 13:09:42 +0800

    using logger instead of sysouts

--- README.md
+++ README.md
@@ -13,3 +13,23 @@ $ mvn clean package -DskipTests -Dmaven.test.skip=true
 ```
 $ MOVIE_FUN_URL=http://moviefun.example.com mvn test
 ```
+
+## Run the spring boot application
+
+1. Start minio
+    ```sh
+    minio server ~/shared
+    ```
+2. Export the `MINIO_ACCESS_KEY` and `MINIO_SECRET_ACCESS_KEY` from the output of the previous command
+    ```sh
+    export MINIO_ACCESS_KEY=VALUE_FROM_PREVIOUS_COMMAND
+    export MINIO_SECRET_ACCESS_KEY=VALUE_FROM_PREVIOUS_COMMAND
+    ```
+3. Setup the `VCAP_SERVICES` environment variable
+    ```sh
+    export VCAP_SERVICES="{\"aws-s3\": [{\"credentials\": {\"access_key_id\": \"${MINIO_ACCESS_KEY:?missing}\", \"bucket\": \"movie-fun-course\", \"secret_access_key\": \"${MINIO_SECRET_ACCESS_KEY:?missing}\"}, \"label\": \"aws-s3\", \"name\": \"moviefun-s3\"}],\"p-mysql\": [{\"credentials\": {\"jdbcUrl\": \"jdbc:mysql://127.0.0.1:3306/movies?user=root\"}, \"name\": \"movies-mysql\"}]}"
+    ```
+4. Run spring boot
+    ```sh
+    mvn spring-boot:run
+    ```
--- src/main/java/org/superbiz/moviefun/Application.java
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -3,6 +3,7 @@ package org.superbiz.moviefun;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +12,8 @@ import org.springframework.context.annotation.Bean;
 import org.superbiz.moviefun.blobstore.BlobStore;
 import org.superbiz.moviefun.blobstore.S3Store;
 import org.superbiz.moviefun.blobstore.ServiceCredentials;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Application {
@@ -27,6 +30,13 @@ public class Application {
     @Bean
     ServiceCredentials serviceCredentials(@Value("${vcap.services}") String vcapServices) {
         return new ServiceCredentials(vcapServices);
+    }
+
+    @Bean
+    public DataSource dataSource(ServiceCredentials serviceCredentials) {
+        MysqlDataSource ds = new MysqlDataSource();
+        ds.setURL(serviceCredentials.getCredential("movies-mysql", "p-mysql", "jdbcUrl"));
+        return ds;
     }
 
     @Bean
@@ -47,4 +57,5 @@ public class Application {
 
         return new S3Store(s3Client, s3BucketName);
     }
+
 }
